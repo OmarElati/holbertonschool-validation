d57fcee48bc491d5be433f97e6e621934356d68e
OmarElati <5796@holbertonstudents.com>
2023-05-12 20:25:30 +0100

    task 4

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,35 +1,14 @@
 #!/bin/bash
 
-set -e
-
 # Update the package lists
 apt-get update
 
 # Install required packages
-apt-get install -y curl git
+apt-get install -y hugo make
 
-# Install Hugo
-HUGO_VERSION=0.88.1
-curl -L -o hugo.tar.gz "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz"
-tar -xf hugo.tar.gz
-mv hugo /usr/local/bin/
-
-# Install Make
-apt-get install -y make
-
-# Run the build command
-make build
-
-# Clone the project repository
-git clone https://github.com/OmarElati/holbertonschool-validation.git
-
-# Navigate to the project directory
-cd holbertonschool-validation
-
-# Verify the presence of the generated website
-if [ -f ./dist/index.html ]; then
-    echo "SETUPOK: Website generated successfully."
+# Verify if the installation was successful
+if command -v hugo >/dev/null 2>&1 && command -v make >/dev/null 2>&1; then
+  echo "SETUPOK"
 else
-    echo "SETUPERROR: Website generation failed."
-    exit 1
+  echo "SETUPERROR"
 fi
