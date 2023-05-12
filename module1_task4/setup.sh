5a5ac32cdc5442bb7a178ed006b7b32052ee5837
OmarElati <5796@holbertonstudents.com>
2023-05-12 21:07:14 +0100

    task 4

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -4,7 +4,15 @@
 apt-get update
 
 # Install required packages
-apt-get install -y hugo make
+apt-get install -y curl make
+
+# Download and install Hugo
+HUGO_VERSION="0.88.1"
+HUGO_BINARY="/usr/local/bin/hugo"
+if [ ! -f "$HUGO_BINARY" ]; then
+  curl -L "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz" | tar -xz
+  mv hugo "$HUGO_BINARY"
+fi
 
 # Verify if the installation was successful
 if command -v hugo >/dev/null 2>&1 && command -v make >/dev/null 2>&1; then
